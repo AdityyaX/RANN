268c1522d6a606e45fd4f360c794f9a73797df07
NEHA MAURYA <nehav3402@gmail.com>
2023-01-27 15:39:59 +0530

    made url links for the events

--- home/views.py
+++ home/views.py
@@ -6,3 +6,52 @@ def index(request):
 
 def test(request):
     return render(request, 'test.html')
+
+def cricket(request):
+    return render(request, 'cricket.html')
+
+def football(request):
+    return render(request, 'football.html')
+
+def table_tennis(request):
+    return render(request, 'table_tennis.html')
+
+def lawn_tennis(request):
+    return render(request, 'lawn_tennis.html')
+ 
+def volleyball(request):
+    return render(request, 'volleyball.html')
+ 
+def basketball(request):
+    return render(request, 'basketball.html')
+ 
+def badminton(request):
+    return render(request, 'badminton.html')
+ 
+def pool(request):
+    return render(request, 'pool.html')
+ 
+def carrom(request):
+    return render(request, 'carrom.html')
+ 
+def chess(request):
+    return render(request, 'chess.html')
+ 
+def shot_put(request):
+    return render(request, 'shot_put.html')
+ 
+def athletics(request):
+    return render(request, 'athletics.html')
+ 
+def kabbadi(request):
+    return render(request, 'kabbadi.html')
+ 
+def karate(request):
+    return render(request, 'karate.html')
+ 
+def yoga(request):
+    return render(request, 'yoga.html')
+ 
+def online_games(request):
+    return render(request, 'online_games.html')
+ 
